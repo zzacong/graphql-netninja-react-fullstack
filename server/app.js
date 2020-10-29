b6e5a714af3aef97a188ffbea10424baf590afa0
zzacong <ongzz100@gmail.com>
2020-10-29 12:17:25 +1100

    create raect app

--- server/app.js
+++ server/app.js
@@ -23,7 +23,7 @@ app.use(
   })
 )
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5000
 app.listen(PORT, () =>
   console.log(`Server is listening on http://localhost:${PORT}`)
 )
